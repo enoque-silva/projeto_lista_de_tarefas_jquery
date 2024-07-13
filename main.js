588acdbe5657ec01f82e2982b63858be04db61ca
enoque <eu.enoqueribeiro@gmail.com>
2024-07-13 18:13:47 -0300

    fix: permiti o clique de confirmação na Div

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-// Aguarda o evento de envio do formulário
-$('form').on('submit', function(evento){
-    // Impede o comportamento padrão de envio do formulário
+// Espera o formulário ser enviado
+$('form').on('submit', function(evento) {
+    // Impede que o formulário seja enviado de verdade (a página não recarrega)
     evento.preventDefault();
     
-    // Obtém o valor do campo de entrada de nova tarefa
+    // Pega o texto que a pessoa digitou no campo de nova tarefa
     const novaTarefa = $('#campo-nome-da-tarefa').val();
     
-    // Cria um novo elemento <li> com a classe 'novaTarefa'
+    // Cria um novo item de lista <li> com uma classe chamada 'novaTarefa'
     const novaLinhaLista = $(`<li class="novaTarefa"></li>`);
 
-    // Cria um botão (ou âncora) com o texto da nova tarefa e o adiciona à nova linha de lista
+    // Cria um link (ou botão) com o texto da nova tarefa e coloca dentro do novo item de lista
     const botaoTarefa = $(`<a>${novaTarefa}</a>`).appendTo($('<div></div>').appendTo(novaLinhaLista));
 
-    // Adiciona um evento de clique ao botão da tarefa para alternar a classe 'tarefaRealizada'
-    botaoTarefa.on('click', function(){
-        $(this).toggleClass('tarefaRealizada');
+    // Adiciona uma função que será chamada quando a nova tarefa for clicada
+    novaLinhaLista.on('click', function() {
+        // Alterna a classe 'tarefaRealizada' no link (ou botão) da tarefa
+        $(this).find('a').toggleClass('tarefaRealizada');
     });
-    
-    // Adiciona a nova linha de lista ao final da lista <ul>
+
+    // Adiciona o novo item de lista no final da lista <ul>
     $(novaLinhaLista).appendTo('ul');
 
-    // Seleciona o elemento com id 'tarefas-titulo' e aplica uma animação de fadeIn com duração de 500ms
+    // Seleciona o elemento com o id 'tarefas-titulo' e faz ele aparecer suavemente em 400ms
     $('#tarefas-titulo').fadeIn(400, function() {
-        // Quando a animação do título terminar, inicia a animação de fadeIn da nova linha de lista com duração de 1000ms
+        // Depois que o título apareceu, faz a nova tarefa aparecer suavemente em 1000ms
         $(novaLinhaLista).fadeIn(1000);
     });
     
-    // Limpa o valor do campo de entrada de nova tarefa após o envio do formulário
+    // Limpa o campo de nova tarefa depois que o formulário é enviado
     $('#campo-nome-da-tarefa').val('');
 });
